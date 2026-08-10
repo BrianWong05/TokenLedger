01b5a38aacfda5129ca854bd29b718b3d07d9f43
BrianWong05 <wongtiniaomacau@gmail.com>
2026-08-10 13:52:49 +1000

    fix(windows): prevent STATUS_ENTRYPOINT_NOT_FOUND in cargo test

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,13 +1,8 @@
 fn main() {
-    for (key, value) in std::env::vars() {
-        if key.starts_with("CARGO") {
-            println!("cargo:warning={}={}", key, value);
-        }
+    let mut attributes = tauri_build::Attributes::new();
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
+        let windows = tauri_build::WindowsAttributes::new_without_app_manifest();
+        attributes = attributes.windows_attributes(windows);
     }
-
-    if std::env::var("CARGO_CFG_TEST").is_ok() {
-        return;
-    }
-
-    tauri_build::build();
+    tauri_build::try_build(attributes).expect("failed to run tauri-build");
 }
